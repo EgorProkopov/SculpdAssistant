72e1cfb16cee1eff75dbc71dffd371faaef8ef97
EgorProkopov <egorprokopov216@gmail.com>
2025-08-22 15:52:12 +0300

    add placeholders to ui, change ui elements size

--- ui/app.py
+++ ui/app.py
@@ -1,5 +1,6 @@
 import json
 import os
+from pathlib import Path
 
 import dotenv
 import gradio as gr
@@ -37,37 +38,37 @@ def generate_next_week(
 
 
 if __name__ == "__main__":
-    dotenv.load_dotenv()
+    root_dir = Path(__file__).resolve().parent.parent
     user_info_placeholder = '{\n"email": "newuser@example.com", \n"image": "http://example.com/image.jpg", \n"name": "New User", \n"gender": "male", \n"birthday": "1995-05-15T00:00:00Z", \n"height": 180.5, \n"height_type": "cm", \n"weight": 75.2, \n"weight_type": "kg", \n"fitness_level": "advanced", \n"improve_body_parts": ["none"], \n"exercise_limitations": ["no_overhead_pressing", "no_squatting", "no_hip_hinge_movements"], \n"nutrition_goal": "maintain_weight", \n"equipment_list": ["barbell", "dumbbells", "machines", "cables"], \n"training_days": 6, \n"workout_time": 80 }'
     previous_week_placeholder = '{}'
 
-    first_week_prompt_text_area = gr.TextArea(label="Program Generation Rules")
-    first_week_prompt_placeholder = os.getenv("")
-    first_week_prompt_text_area.value = "need a text placeholder"
+    first_week_prompt_text_area = gr.TextArea(label="Program Generation Rules", lines=22)
+    first_week_prompt_path = root_dir / r"data/prompt_rules/first_week/prompt_placeholder.txt"
+    first_week_prompt_text_area.value = first_week_prompt_path.read_text()
 
     first_week_interface = gr.Interface(
         fn=generate_first_week,
         inputs=[
-            gr.TextArea(label="User Information", placeholder=user_info_placeholder),
-            gr.TextArea(label="Scanner Info"),
-            gr.TextArea(label="Program Generation Rules")
+            first_week_prompt_text_area,
+            gr.TextArea(label="User Information", placeholder=user_info_placeholder, lines=18),
+            gr.TextArea(label="Scanner Info", lines=18),
         ],
         outputs=gr.Textbox(label="Training Plan"),
         title="First Week Generation",
         description="Upload user information and scanner output to receive first week training plan",
     )
 
-    next_week_prompt_text_area = gr.TextArea(label="Program Generation Rules")
-    next_week_prompt_placeholder = os.getenv("")
-    next_week_prompt_text_area.value = "need a text placeholder"
+    next_week_prompt_text_area = gr.TextArea(label="Program Generation Rules", lines=22)
+    next_week_prompt_path = root_dir / r"data/prompt_rules/next_week/prompt_placeholder.txt"
+    next_week_prompt_text_area.value = next_week_prompt_path.read_text()
 
     next_week_interface = gr.Interface(
         fn=generate_next_week,
         inputs=[
-            gr.TextArea(label="User Information", placeholder=user_info_placeholder),
-            gr.Textbox(label="Feedback Key", placeholder="hard"),
-            gr.TextArea(label="Previous Week JSON", placeholder=previous_week_placeholder),
-            next_week_prompt_text_area
+            next_week_prompt_text_area,
+            gr.TextArea(label="User Information", placeholder=user_info_placeholder, lines=18),
+            gr.Textbox(label="Feedback Key", placeholder="hard", lines=1),
+            gr.TextArea(label="Previous Week JSON", placeholder=previous_week_placeholder, lines=18)
         ],
         outputs=gr.Textbox(label="Training Plan"),
         title="Next Week Generation",
